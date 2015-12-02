cf19aedc699fa109292c0068f81cb2967829a0c1
Murray Steele <muz@h-lame.com>
2015-12-02 09:39:13 +0000

    Add shell script to setup ENV vars for Makefile
    Assumes you've checked out all our implementations of Jack/Hack stuff to a sibling of this repo.

---
+++ hackfrastructure.sh
@@ -0,0 +1,4 @@
+export HACK_ASSEMBLER=../hack-assembler/bin/assembler
+export VM_TRANSLATOR=../vm-translator/bin/translator
+export JACK_COMPILER=../jack-compiler/bin/compile_vm
+export HACK_JS=../hack-js/bin/run
