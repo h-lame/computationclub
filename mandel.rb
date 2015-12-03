b2ac8598a0e7eab55ec3948df7701948c7860796
Murray Steele <muz@h-lame.com>
2015-12-03 09:46:51 +0000

    Add command line options to configure output
    We also tweak the pixels_for_count function to scale the count to the available pixels, instead of using modulo which would cycle the pixels.  This means we also tweak the arrangement of the pixels.

--- mandel.rb
+++ mandel.rb
@@ -1,16 +1,31 @@
 
-def pixels_for_count(count)
-  # count > 4 ? '#' : '.'
-  [' ','.','-','/','|','<','{','%','&','@','#'][count % 10]
-end
 
-acorn = -2.4
-bcorn = -1.2
-size = 3.1
+require 'optparse'
+
+options = { width: 600, height: 600, acorn: -2, bcorn: -2, size: 4, iterations: 100 }
+OptionParser.new do |opts|
+  opts.banner = "Usage: mandel.rb [options]"
+
+  opts.on("-wWIDTH", "Output width") { |w| options[:width] = Integer(w) }
+  opts.on("-hHEIGHT", "Output height") { |h| options[:height] = Integer(h) }
+  opts.on("-aACORN", "Mandelbrot viewport top right corner x") { |a| options[:acorn] = Float(a) }
+  opts.on("-bBCORN", "Mandelbrot viewport top right corner y") { |b| options[:bcorn] = Float(b) }
+  opts.on("-sSIZE", "Mandelbrot viewport size") { |s| options[:size] = Float(s) }
+  opts.on("-iITERATIONS", "Mandelbrot iterations") { |i| options[:iterations] = Integer(i) }
+
+end.parse!
+
+acorn = options[:acorn]
+bcorn = options[:bcorn]
+width = options[:width].to_f
+height = options[:height].to_f
+size = options[:size]
+iterations = options[:iterations]
+
 pixels = []
-(1..150).each do |j|
+(1..width).each do |j|
   pixels[j-1] = []
-  (1..150).each do |k|
+  (1..height).each do |k|
     ca = acorn + ((j * size) / 150.0)
     cb = bcorn + ((k * size) / 150.0)
     count = zx = zy = 0
@@ -23,8 +38,11 @@ pixels = []
       zy = (2 * zxy) + cb
       zx = xtemp + ca
     end while (count < 100) && ((zxx + zyy) < 4)
-    pixels[j-1][k-1] = pixels_for_count(count)
+    pixels[j-1][k-1] = count
   end
 end
 
-puts pixels.transpose.map {|r| r.join}.join("\n")
+PIXEL_DATA = ['.','-','/','|','<','{','%','&','@','#',' ']
+pixel_for_count = ->(x) { PIXEL_DATA[(((PIXEL_DATA.size-1) * x) / iterations)] }
+
+puts pixels.transpose.map { |r| r.map { |c| pixel_for_count[c] }.join }.join("\n")
